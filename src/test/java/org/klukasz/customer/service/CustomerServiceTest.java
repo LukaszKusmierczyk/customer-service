25aae978e276dea8677d5e9f503220d69f09eb0c
Lukasz Kusmierczyk <ext.lukasz.kusmierczyk@kyriba.com>
2021-10-14 11:11:36 +0200

    test added

---
+++ src/test/java/org/klukasz/customer/service/CustomerServiceTest.java
@@ -0,0 +1,57 @@
+package org.klukasz.customer.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.klukasz.customer.model.Customer;
+import org.klukasz.customer.model.CustomerDTO;
+import org.klukasz.customer.repository.CustomerRepository;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class CustomerServiceTest {
+
+    private static final String CODE = "Coca-Cola";
+    private static final long ID = 100;
+    private static final String NAME = "Coca Cola inc.";
+
+    @Mock
+    private CustomerRepository customerRepository;
+
+    @Mock
+    private Customer customer;
+
+    private CustomerService customerService;
+
+    private CustomerDTO customerDTO;
+
+    @BeforeEach
+    public void initEach() {
+        customerService = new CustomerService(customerRepository);
+        customerDTO = new CustomerDTO();
+        customerDTO.setId(ID);
+        customerDTO.setCode(CODE);
+        customerDTO.setName(NAME);
+    }
+
+    @Test
+    void should_return_customer() {
+        //given
+        when(customerRepository.findByCustomerCode(any())).thenReturn(customer);
+        when(CustomerDTO.of(customer)).thenReturn(customerDTO);
+        //when
+        CustomerDTO customerDTO = customerService.getCustomer("c001");
+
+        //then
+        assertEquals(CODE, customerDTO.getCode());
+        assertEquals(ID, customerDTO.getId());
+        assertEquals(NAME, customerDTO.getName());
+    }
+}
